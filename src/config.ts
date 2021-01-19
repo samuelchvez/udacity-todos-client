e679f0c355598a95c1680944a9b41922522b4dee
Samuel Chávez <schavezfuentes@gmail.com>
2021-01-19 01:07:36 -0600

    Test client for serverless project

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'szlmm09k3h';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-xpq7j4qr.us.auth0.com',
+  clientId: '2U9FkpD4hmCeyOuarKWVWNPPttM634Ss',
+  callbackUrl: 'http://localhost:3000/callback',
+}
